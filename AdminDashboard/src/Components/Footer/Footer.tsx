978fa9e862abefb24044bd8e455bc9fafc923f8d
vchavkov <vchavkov@chavkov.com>
2024-10-25 14:28:46 +0000

    Bosch, Inc.

--- AdminDashboard/src/Components/Footer/Footer.tsx
+++ AdminDashboard/src/Components/Footer/Footer.tsx
@@ -68,7 +68,7 @@ const DashboardFooter: () => JSX.Element = () => {
     <>
       <Footer
         className="bg-white h-16 inset-x-0 bottom-0 px-8"
-        copyright="Bosch, Inc."
+        copyright="Bosch Inc.."
         links={[
           {
             title: "Help and Support",
