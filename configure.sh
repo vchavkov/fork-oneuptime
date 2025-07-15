e3f2aef55a86a7ddb83827cff1cf23b9007d5c73
vchavkov <vchavkov@chavkov.com>
2025-07-15 12:57:26 +0000

    fix typo

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 set -a
 
-# Create .env file if it does not exist. 
+# Create .env file if it does not exist.
 touch config.env
 
 #Run a scirpt to merge config.env.tpl to config.env
@@ -11,7 +11,7 @@ node ./Scripts/Install/MergeEnvTemplate.js
 # Load env values from config.env
 export $(grep -v '^#' config.env | xargs)
 
-# Write env vars in config files. 
+# Write env vars in config files.
 for directory_name in $(find . -maxdepth 1 -type d) ; do
     if [ -f "$directory_name/Dockerfile.tpl" ]; then
         cat $directory_name/Dockerfile.tpl | gomplate > $directory_name/Dockerfile
